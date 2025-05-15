362b1c60c2a9a72d8864ef0d5ea5ddb223bc76d3
ShashankSS1205 <Shashankshekharsingh1205@gmail.com>
2025-05-15 19:59:29 +0530

    code added

--- app.py
+++ app.py
@@ -1669,4 +1669,6 @@ def test_gcs():
         }), 500
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    # app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
